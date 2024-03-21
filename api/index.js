c40ee3dbe13675d19ee4462cd925c2c4baf31128
mashhood2000 <mashhood982009@gmail.com>
2024-03-22 01:45:54 +0500

    Delete, update and some changes  user routes> useroute

--- api/index.js
+++ api/index.js
@@ -1,36 +1,45 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user.routes.js';
-import authRouter from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
 
- 
-mongoose.connect("mongodb+srv://mashhood:mashhood@mern-estate.qbgbhfr.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate").then(()=>{console.log('Connected to MongoDB');
-})
-.catch((err) => {
+mongoose
+  .connect("mongodb+srv://mashhood:mashhood@mern-estate.qbgbhfr.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
+
+  const __dirname = path.resolve();
+
 const app = express();
-app.use(express.json())
-app.listen(3000,() => {
-    console.log('server is running on port 3000!!!');
-}
-);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-
-    const message = err.message || 'internal Server error';
-    return res.status(statusCode).json({
-        sucess: false,
-        statusCode,
-        message,
-
-    });
 
 
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
