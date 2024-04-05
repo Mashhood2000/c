2fc6da84c29a88f5d85e4df11ce941d83f902850
mashhood2000 <mashhood982009@gmail.com>
2024-04-05 06:02:07 +0500

    Admin functionalities, isadmin added in delete and update functions, verify user checked, signup can be done as either admin or simple user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,7 +9,7 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  if (!(req.user.id === req.params.id || req.user.isAdmin))
     return next(errorHandler(401, 'You can only update your own account!'));
   try {
     if (req.body.password) {
@@ -38,9 +38,14 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account!'));
   try {
+    const userToDelete = await User.findById(req.params.id);
+    
+    // Check if the user making the request is the user to be deleted or an admin
+    if (!(req.user.id === req.params.id || req.user.isAdmin)) {
+      return next(errorHandler(401, 'You are not authorized to delete this user!'));
+    }
+    
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie('access_token');
     res.status(200).json('User has been deleted!');
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,15 +1,26 @@
 import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+import User from '../models/user.model.js';
 
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
+export const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
 
     req.user = user;
     next();
-  });
+  } catch (error) {
+    console.log("i am here");
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return res.status(403).json({ message: 'Access forbidden' });
+  }
+  next();
 };
